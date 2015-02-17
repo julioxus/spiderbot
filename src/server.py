799467457b70487debb3d67b11fc2c2eada9f2e1
julioxus <julioxus@gmail.com>
2015-02-17 14:46:17 +0100

    Iniciando configuración del proyecto

---
+++ src/server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -⁻- coding: UTF-8 -*-
+
+import webapp2
+import jinja2
+import os
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import ndb
+
+# Declaración del entorno de jinja2 y el sistema de templates.
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    
+    
+    def get(self):
+        template_values={}
+        template = JINJA_ENVIRONMENT.get_template('template/index.html')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(template.render(template_values))
+
+
+urls = [('/',MainPage),
+       ]
+
+application = webapp2.WSGIApplication(urls, debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
